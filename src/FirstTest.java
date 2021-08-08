aa524021d0cec5e433cf4ce70135f37df4b1d96c
azadiraka <spaiss534@gmail.com>
2021-08-08 19:17:39 +0300

    Update FirstTest.java
    4 lesson Ex5: addTwoArticlesToSaved()

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -228,6 +228,96 @@ public class FirstTest {
     }
 
     @Test
+    public void addTwoArticlesToSaved() {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                "Cannot find a SKIP button",
+                1);
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find the Search Wikipedia input",
+                2);
+        String first_search_line = "Harry Potter";
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                first_search_line,
+                "Cannot find search input",
+                2);
+        String first_article_locator = "//*[@text='Fantasy literature series by J.K. Rowling']";
+        waitForElementAndClick(
+                By.xpath(first_article_locator),
+                "Cannot find the article by the request" + first_search_line,
+                5);
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/article_menu_bookmark"),
+                "Cannot find bookmark button",
+                5);
+        waitForElementAndClick(
+                By.id("Navigate up"),
+                "Cannot find <- button to move to search results list",
+                1);
+        waitForElementAndClick(
+                By.id("Clear query"),
+                "Cannot find 'X' button to clear search input",
+                2);
+        String second_search_line = "Star Wars";
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                second_search_line,
+                "Cannot find search input",
+                2);
+        String second_article_locator = "//*[@text='1977 American epic space-opera film directed by George Lucas']";
+        waitForElementAndClick(
+                By.xpath(second_article_locator),
+                "Cannot find the article by the request" + second_search_line,
+                5);
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/article_menu_bookmark"),
+                "Cannot find bookmark button",
+                5);
+        waitForElementAndClick(
+                By.id("Navigate up"),
+                "Cannot find <- button to move to search results list",
+                1);
+        waitForElementAndClick(
+                By.xpath("//*[@class='android.widget.ImageButton']"),
+                "Cannot find <- button to move to main screen",
+                5);
+        waitForElementAndClick(
+                By.id("Saved"),
+                "Cannot find Saved button",
+                1);
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/item_title'][@text='Saved']"),
+                "Cannot find 'Saved' list",
+                5);
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_description']" + first_article_locator.substring(3)),
+                "Cannot find first article in the 'Saved' list found by the request" + first_search_line,
+                5);
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_description']" + second_article_locator.substring(3)),
+                "Cannot find second article in the 'Saved' list found by the request" + second_search_line,
+                5);
+        swipeElementToLeft(
+                By.xpath(second_article_locator),
+                "Cannot find second article in the 'Saved' list found by the request" + second_search_line);
+        waitForElementNotPresented(
+                By.xpath(second_article_locator),
+                "Cannot delete second article in the 'Saved' list found by the request" + second_search_line,
+                5);
+        waitForElementAndClick(
+                By.xpath(first_article_locator),
+                "Cannot find first article in the 'Saved' list found by the request" + first_search_line,
+                5);
+        assertElementHasText(
+                By.id("pcs-edit-section-title-description"),
+                "Fantasy literature series by J.K. Rowling",
+                "Cannot find expected text in first article found by the request" + first_search_line,
+                5);
+    }
+
+    @Test
     public void testAmountOfNonEmptySearch(){
         waitForElementAndClick(
                 By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
